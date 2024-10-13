ba7a46980a576e46b82f2a2fbf3ae4286d38e356
Alen Zgurić <zguric.alen@hotmail.com>
2024-10-13 10:59:41 +0200

    add komponenet on kitchepage and style

--- src/pages/KitchensPage.jsx
+++ src/pages/KitchensPage.jsx
@@ -1,20 +1,26 @@
-import React from 'react'
-import { Helmet } from 'react-helmet'
-import NavBar from '../components/NavBar'
+import React from 'react';
+import KitchenComponent from '../components/KitchenComponent';
+import { kitchensData } from '../styles/utils/kitchensData';
+import NavBar from '../components/NavBar';
+import { Helmet } from 'react-helmet';
+
+import '../styles/pages/KitchensPage.css';
 
 const KitchensPage = () => {
+  
   return (
-    <div className='kitchens-page'>
-        <Helmet>
-
-        </Helmet>
-        <NavBar/>
-        <main style={{height: '100vh'}}>
-            <h1>Kitchens</h1>
-
-        </main>
-        </div>
-  )
-}
+    <div className="kitchens-page">
+      <Helmet>
+        <title>Kuhinje</title>
+      </Helmet>
+      <NavBar />
+      <main>
+        {kitchensData.map((kitchen, index) => (
+          <KitchenComponent key={index} kitchen={kitchen} />
+        ))}
+      </main>
+    </div>
+  );
+};
 
 export default KitchensPage;
