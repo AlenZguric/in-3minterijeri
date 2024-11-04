9759475178fdedce9a3edd9d22c14d4b687bdb07
Alen Zgurić <zguric.alen@hotmail.com>
2024-11-04 14:01:12 +0100

    "Updated CookieNotice component styles and OurBusiness component styles and CSS map"

--- src/components/CookieNotice.jsx
+++ src/components/CookieNotice.jsx
@@ -4,13 +4,12 @@ import { useCookies } from '../contexts/CookieContekst';
 
 const CookieNotice = () => {
   const { cookiesAccepted, acceptCookies } = useCookies();
-
   const navigate = useNavigate();
 
   if (cookiesAccepted) return null;
 
   const handlePrivacyClick = () => {
-    navigate('/politika-privatnosti'); // navigacija na stranicu Politike privatnosti
+    navigate('/politika-privatnosti');
   };
 
   return (
@@ -22,11 +21,10 @@ const CookieNotice = () => {
             Slažem se
           </button>
           <button style={styles.acceptButton} onClick={handlePrivacyClick}>
-          <Link to="/politika-privatnosti" style={styles.privacyLink}>
-            Politika privatnosti
-          </Link>
+            <Link to="/politika-privatnosti" style={styles.privacyLink}>
+              Politika privatnosti
+            </Link>
           </button>
-          
         </div>
       </div>
     </div>
@@ -34,49 +32,47 @@ const CookieNotice = () => {
 };
 
 const styles = {
-    overlay: {
-      position: 'fixed',
-      top: 0,
-      left: 0,
-      width: '100%',
-      height: '100%',
-      backgroundColor: 'rgba(0, 0, 0, 0.7)',
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      zIndex: 1000,
-    },
-    cookieContainer: {
-      backgroundColor: 'white',
-      padding: '20px',
-      borderRadius: '5px',
-      textAlign: 'center',
-      width: '100%',
-      position: 'fixed',
-      bottom: '0',
-     // justifyContent: 'center',
-    },
-    buttonContainer: {
-      marginTop: '10px',
-
-    },
-    acceptButton: {
-      backgroundColor: '#4CAF50',
-      color: 'white',
-      border: 'none',
-      padding: '10px 20px',
-      cursor: 'pointer',
-      borderRadius: '5px',
-      marginRight: '10px',
-      textDecoration: 'none',
-    },
-    
-    privacyLink: {
-      color: 'white',
-      border: 'none',
-      textDecoration: 'none',
-      cursor: 'pointer',
-    },
-  };
+  overlay: {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'rgba(0, 0, 0, 0.7)',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    zIndex: 1000,
+  },
+  cookieContainer: {
+    backgroundColor: 'white',
+    padding: '20px',
+    borderRadius: '5px',
+    textAlign: 'center',
+    width: '80%', // prilagođeno za različite veličine ekrana
+    maxWidth: '500px',
+    boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.2)',
+  },
+  buttonContainer: {
+    marginTop: '10px',
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  acceptButton: {
+    backgroundColor: '#4CAF50',
+    color: 'white',
+    border: 'none',
+    padding: '10px 20px',
+    cursor: 'pointer',
+    borderRadius: '5px',
+    marginRight: '10px',
+    textDecoration: 'none',
+  },
+  privacyLink: {
+    color: 'white',
+    textDecoration: 'none',
+    cursor: 'pointer',
+  },
+};
 
 export default CookieNotice;
