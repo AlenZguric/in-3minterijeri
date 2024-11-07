37c1ae80d56c45589f05ed121c3c6ab45c124a36
Alen Zgurić <zguric.alen@hotmail.com>
2024-11-07 01:50:53 +0100

    add images for gallery

---
+++ src/assets/images/fullsize/Paneli/preimenuj.bat
@@ -0,0 +1,18 @@
+@echo off
+setlocal enabledelayedexpansion
+
+REM Definiraj početni broj
+set "counter=1"
+
+REM Idi u folder gdje se nalaze .webp slike
+cd /d "E:\PROJEKTI\in-3m\in-3minterijeri\src\assets\images\fullsize\Paneli"
+
+REM Petlja kroz sve .webp datoteke u folderu
+for %%f in (*.webp) do (
+    REM Preimenuj datoteku u broj.webp
+    ren "%%f" "!counter!.webp"
+    set /a counter+=1
+)
+
+echo Preimenovanje završeno!
+pause
